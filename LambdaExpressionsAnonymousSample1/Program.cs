a1593fca269d2972f6bbb8f51a4f397fb08f5f71
camorim <camorim@vitait@.com>
2018-07-09 11:06:26 -0300

    Add project files.

---
+++ LambdaExpressionsAnonymousSample1/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpressionsAnonymousSample1
+{
+    public class Cachorro
+    {
+        public string Nome{ get; set; }
+        public int Idade { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new List<Cachorro>() {
+                new Cachorro { Nome = "Neymar", Idade = 4 },
+                new Cachorro { Nome = "Fernandinho", Idade = 9 },
+                new Cachorro { Nome = "Miranda", Idade = 3 }
+            };
+
+            var animalLista = animal.Select(x => new { Idade = x.Idade, NomeAnimal = x.Nome });
+
+            foreach (var item in animalLista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
